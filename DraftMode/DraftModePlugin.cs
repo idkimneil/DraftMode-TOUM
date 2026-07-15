fd129c44065cf88644a9e06ab7e4f616b04968eb
neilmjain <69025896+neilmjain@users.noreply.github.com>
2026-07-15 16:10:43 -0400

    update to 2.0.0

--- DraftMode/DraftModePlugin.cs
+++ DraftMode/DraftModePlugin.cs
@@ -46,7 +46,7 @@ namespace DraftModeTOUM
         {
             public const string PLUGIN_GUID = "com.draftmodetoum.mod";
             public const string PLUGIN_NAME = "Draft Mode";
-            public const string PLUGIN_VERSION = "1.2.1";
+            public const string PLUGIN_VERSION = "2.0.0";
         }
 
         [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.OnDisconnected))]
